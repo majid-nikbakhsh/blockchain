12c8cd13cf800eddf73f5490b4674102f712adb2
me <majid.nikbakhsh@gmail.com>
2020-09-13 11:26:44 +0430

    m

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	bc := NewBlockchain()
-	bc.AddBlock("Send 1 BTC to Ivan")
+	bc := NewBlockchain().
+		bc.AddBlock("Send 1 BTC to Ivan")
 	bc.AddBlock("Send 2 more BTC to Ivan")
 
 	for _, block := range bc.blocks {
