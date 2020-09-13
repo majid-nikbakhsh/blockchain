0e0a266012aaacadfc72e90bcee7841fe461fdc5
me <majid.nikbakhsh@gmail.com>
2020-09-13 11:32:24 +0430

    m

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 )
 
 func main() {
-	bc := NewBlockchain().
-		bc.AddBlock("Send 1 BTC to Ivan")
+	bc := NewBlockchain()
+	bc.AddBlock("Send 1 BTC to Ivan")
 	bc.AddBlock("Send 2 more BTC to Ivan")
 
 	for _, block := range bc.blocks {
